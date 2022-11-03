910cc61ff605cb7bba929ff4f5e82123fda5b79d
Denis Smirnov <gonzru@gmail.com>
2022-11-04 00:27:38 +0500

    add models

---
+++ src/models/movie.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const { linkRegExp } = require('../utils/validators');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    match: linkRegExp,
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    match: linkRegExp,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    match: linkRegExp,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const WrongLoginOrPasswordError = require('../errors/wrongLoginOrPasswordError');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new WrongLoginOrPasswordError();
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new WrongLoginOrPasswordError();
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
